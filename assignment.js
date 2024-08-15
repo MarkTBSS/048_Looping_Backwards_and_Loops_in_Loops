e58fb1a2285c4020986ca717b8f637a48d6fbecd
MarkTBSS <marktbss@gmail.com>
2024-08-15 08:02:35 +0700

    [ADD] First Commit

---
+++ assignment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'],
+['Norway', 'Sweden', 'Russia']
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++)
+    for (let y = 0; y < listOfNeighbours[i].length; y++)
+        console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
